2633324b08932d3d75ba7c757c45bf0b5c8527da
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2022-10-14 21:49:56 +0000

    [Add]book index完成,create機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,10 +7,10 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new
-    @book.user_id = current_user.id
-    if @book.save
-      redirect_to books_path
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    if book.save
+      redirect_to book_path(book)
     else
       render :new
     end
@@ -23,5 +23,9 @@ class BooksController < ApplicationController
   def edit
   end
 
+  private
 
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
