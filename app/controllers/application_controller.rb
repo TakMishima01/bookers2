47e0a16d5f2914f51441cad87305488071d8fbff
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2022-10-16 13:18:10 +0000

    [Add]user_update実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,21 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    about_path
+    flash[:notice] = "ログインが成功しました"
+    user_path(current_user)
   end
 
   def after_sign_out_path_for(resource)
-    about_path
+    root_path
   end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [:name])エラーが出る
+
+    added_attrs = [:user_name, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,6 @@ class UsersController < ApplicationController
     @book_new = Book.new
   end
 
-  def edit
-  end
-
   def create
     book = Book.new(book_params)
     book.user_id = current_user.id
@@ -28,7 +25,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+
   private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
 
   def book_params
     params.require(:book).permit(:title, :body)
